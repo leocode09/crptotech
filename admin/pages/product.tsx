99250d3e14ec7db04160c7d7712892f89c624c26
leocode09 <132523651+leocode09@users.noreply.github.com>
2023-09-11 10:53:44 -0700

    leo2

---
+++ admin/pages/product.tsx
@@ -0,0 +1,10 @@
+const Product = () => {
+    return (
+        <div>
+            <input type="text" />
+            <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, itaque?</h1>
+        </div>
+    )
+}
+
+export default Product;
